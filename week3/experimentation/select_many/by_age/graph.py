00f20a9fe0a201727d5524036fc3394921c5128c
pyo <cspyo98@naver.com>
2024-02-20 03:13:00 +0900

    feat: add graph

---
+++ week3/experimentation/select_many/by_age/graph.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# 데이터를 DataFrame으로 읽어오기
+
+df = pd.read_csv('./select_many_by_age.csv')
+
+# 막대 그래프 그리기
+plt.figure(figsize=(10, 6))
+plt.bar(df['DB'], df['Time'], color='skyblue')
+plt.xlabel('Database')
+plt.ylabel('Time (seconds)')
+plt.title('SELECT * FROM passenger where age= :age')
+plt.show()
