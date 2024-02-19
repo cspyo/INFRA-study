796f5644f05f93c9537b71966384451c4117a706
pyo <cspyo98@naver.com>
2024-02-19 19:15:17 +0900

    feat: env tuple

--- week3/get_env.py
+++ week3/get_env.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 def get_rds():
     load_dotenv()
-    return os.getenv("RDS_HOST"), os.getenv("RDS_PORT"), os.getenv("RDS_DB_NAME"), os.getenv("RDS_USER"), os.getenv("RDS_PASSWORD")
+    return (os.getenv("RDS_HOST"), os.getenv("RDS_PORT"), os.getenv("RDS_DB_NAME"), os.getenv("RDS_USER"), os.getenv("RDS_PASSWORD"))
 
 def get_redshift():
     load_dotenv()
