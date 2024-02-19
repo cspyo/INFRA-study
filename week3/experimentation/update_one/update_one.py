1ab8f3fb49c9f176dd8a02866ecd7f63afa17775
pyo <cspyo98@naver.com>
2024-02-20 02:31:04 +0900

    feat: update name by id

--- week3/experimentation/update_one/update_one.py
+++ week3/experimentation/update_one/update_one.py
@@ -27,7 +27,7 @@ def update_name_by_id():
     redis_insert_time = timeit.timeit(lambda: redis.update_name_by_id('god',passenger), number=1)
     results.append(['redis', redis_insert_time])
 
-    dynamodb_insert_time = timeit.timeit(lambda: dynamodb.update_name_by_id('god',999), number=1)
+    dynamodb_insert_time = timeit.timeit(lambda: dynamodb.update_name_by_id('god',1), number=1)
     results.append(['dynamodb', dynamodb_insert_time])
 
     df = pd.DataFrame(results, columns=['DB', 'Time'])
