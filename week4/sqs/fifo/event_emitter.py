6054d6fd0699b43579ce724f3c91351981b0f4ea
pyo <ubuntu@ip-172-31-86-40.ec2.internal>
2024-02-25 15:33:13 +0000

    feat: sqs fifo

--- week4/sqs/fifo/event_emitter.py
+++ week4/sqs/fifo/event_emitter.py
@@ -2,13 +2,13 @@ import sys
 sys.path.append('/home/ubuntu/code/INFRA-study')
 import boto3
 import time
-from week4.util.get_env import get_sqs_std
+from week4.util.get_env import get_sqs_fifo
 from week4.event_emitter import emit_event
 
 session = boto3.Session(profile_name='pyo')
 sqs = session.client('sqs')
 
-queue_url = get_sqs_std()
+queue_url = get_sqs_fifo()
 
 def publish_events(i):
     timestamp = time.time() 
