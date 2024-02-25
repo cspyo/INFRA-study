15de273c8d0e4541925f8aafc12236d3a551472c
pyo <cspyo98@naver.com>
2024-02-25 21:43:19 +0900

    feat: divide sns emitter

--- week4/sns/event_emitter.py
+++ week4/sns/event_emitter.py
@@ -3,7 +3,7 @@ sys.path.append('/home/ubuntu/code/INFRA-study')
 import boto3
 import time
 from week4.util.get_env import get_sns
-import concurrent.futures
+from week4.event_emitter import emit_event
 
 session = boto3.Session(profile_name='pyo')
 sns = session.client('sns')
@@ -15,15 +15,4 @@ def publish_events(i):
     message = str(timestamp)
     sns.publish(TopicArn=topic_arn, Message=message) 
 
-num_events_per_second = 100
-seconds_for_publish = 60
-
-start_time = time.time()
-with concurrent.futures.ThreadPoolExecutor() as executor:
-    for _ in range(seconds_for_publish):
-        executor.map(publish_events, range(num_events_per_second))
-        time.sleep(1) 
-end_time = time.time()
-
-print("Total publish time", end_time-start_time)
-print("Total publish count", num_events_per_second*seconds_for_publish)
+emit_event(publish_events)
