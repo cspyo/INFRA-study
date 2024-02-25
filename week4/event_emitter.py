7aae71daa7b6cd1a11579653626e3736f334a569
pyo <ubuntu@ip-172-31-86-40.ec2.internal>
2024-02-25 16:26:48 +0000

    feat: add error log

--- week4/event_emitter.py
+++ week4/event_emitter.py
@@ -3,13 +3,16 @@ import concurrent.futures
 
 def emit_event(emit_function):
     num_events_per_second = 100
-    seconds_for_publish = 60
+    seconds_for_publish = 1
 
     start_time = time.time()
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        for _ in range(seconds_for_publish):
-            executor.map(emit_function, range(num_events_per_second))
-            time.sleep(1) 
+    try:
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            for _ in range(seconds_for_publish):
+                executor.map(emit_function, range(num_events_per_second))
+                time.sleep(1) 
+    except Exception as e:
+        print(f"An error occurred in emit_event: {e}")
     end_time = time.time()
 
     print("Total publish time", end_time-start_time)
--- week4/sqs/fifo/event_emitter.py
+++ week4/sqs/fifo/event_emitter.py
@@ -4,6 +4,7 @@ import boto3
 import time
 from week4.util.get_env import get_sqs_fifo
 from week4.event_emitter import emit_event
+from uuid import uuid4
 
 session = boto3.Session(profile_name='pyo')
 sqs = session.client('sqs')
@@ -14,9 +15,15 @@ def publish_events(i):
     timestamp = time.time() 
     message_body = {
         'Message': timestamp,
-        'MessageAttributes': {}
     }
-    sqs.send_message(QueueUrl=queue_url, MessageBody=str(message_body)) 
+    try:
+        sqs.send_message(
+            QueueUrl=queue_url, 
+            MessageBody=str(message_body), 
+            MessageDeduplicationId=str(uuid4()), 
+            MessageGroupId='group1') 
+    except Exception as e:
+        print(f"An error occurred in emit_event: {e}")
 
-
+print(queue_url)
 emit_event(publish_events)
