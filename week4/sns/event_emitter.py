f8aacf20cee59493e105f92e99b7a30e1021f5dc
pyo <cspyo98@naver.com>
2024-02-25 19:06:20 +0900

    feat: use thread

--- week4/sns/event_emitter.py
+++ week4/sns/event_emitter.py
@@ -3,21 +3,24 @@ sys.path.append('/home/ubuntu/code/INFRA-study')
 import boto3
 import time
 from week4.util.get_env import get_sns
-
+import concurrent.futures
 
 session = boto3.Session(profile_name='pyo')
-sns = session.client('sns')
+sns = boto3.client('sns')
 
 topic_arn = get_sns()
 
-def produce_events(topic_arn):
-    for i in range(1 * 1):  
-        timestamp = time.time()
+def publish_events(num_events):
+    for _ in range(num_events):
+        timestamp = time.time() 
         message = str(timestamp)
-        start_time = time.time()
         sns.publish(TopicArn=topic_arn, Message=message) 
-        end_time = time.time()
-        print(end_time-start_time)
-        time.sleep(0.01)
 
-produce_events(topic_arn)
+num_events_per_second = 100
+seconds_for_publish = 60
+total_events = seconds_for_publish * num_events_per_second 
+
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    for _ in range(seconds_for_publish):
+        executor.map(publish_events, [num_events_per_second] * num_events_per_second)
+        time.sleep(1) 
